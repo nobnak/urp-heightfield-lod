94d87c12ab36272a065295c55e12bf9fa731c23e
Nobu Nakata <nobu.nakata@gmail.com>
2026-06-12 14:42:55 +0900

    refactor!: consolidate rig wiring into HeightFieldLayoutHost

--- Packages/jp.nobnak.heightfield-lod/Runtime/Draw/HeightFieldChunkMeshDrawer.cs
+++ Packages/jp.nobnak.heightfield-lod/Runtime/Draw/HeightFieldChunkMeshDrawer.cs
@@ -11,23 +11,18 @@ namespace HeightFieldLod
         [SerializeField] int _sortOrder;
         [SerializeField] bool _castShadows = true;
 
-        HeightFieldLayoutHost _layoutHost;
-        HeightFieldLodCompute _lod;
+        ILodSource _lod;
         IHeightFieldSource _heightSource;
         HeightFieldLayout _layout;
         MaterialPropertyBlock _mpb;
 
-        ILodSource Lod => _lod;
+        void Awake() => _mpb = new MaterialPropertyBlock();
 
-        void Awake()
+        public void SetDependencies(ILodSource lod, IHeightFieldSource heightSource)
         {
-            _mpb = new MaterialPropertyBlock();
-            ResolveRefs();
+            _lod = lod;
+            _heightSource = heightSource;
         }
-
-        void Reset() => ResolveRefs();
-
-        void OnValidate() => ResolveRefs();
 
         public void Configure(HeightFieldLayout layout)
         {
@@ -36,49 +31,23 @@ namespace HeightFieldLod
                 _material.enableInstancing = true;
         }
 
-        public void Release()
-        {
-            RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
-        }
+        void OnEnable() => RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
 
-        void OnEnable()
-        {
-            ResolveRefs();
-            RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
-        }
-
-        void OnDisable() => Release();
+        void OnDisable() => RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
 
         void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
         {
-            if (!ShouldRender(camera))
-                return;
-            var layout = ResolveLayout();
-            if (layout.TexWidth <= 0 || Lod == null || _material == null)
-                return;
+            if (!ShouldRender(camera)) return;
+            if (_layout.TexWidth <= 0 || _lod == null || _material == null) return;
 
             if (_heightSource != null)
-                _heightSource.EnsureUpdated(layout, Time.time);
-            var height = _heightSource != null ? _heightSource.HeightTexture : Lod.HeightTexture;
+                _heightSource.EnsureUpdated(_layout, Time.time);
+            var height = _heightSource != null ? _heightSource.HeightTexture : _lod.HeightTexture;
             if (height == null) return;
 
-            Lod.EnsureUpdated(layout, height);
-            BindMaterialTextures(height, Lod.NormalTexture);
-            DrawLayers(camera, layout);
-        }
-
-        void ResolveRefs()
-        {
-            _layoutHost = GetComponent<HeightFieldLayoutHost>();
-            _lod = GetComponent<HeightFieldLodCompute>();
-            _heightSource = HeightFieldRigUtil.FindHeightSource(gameObject);
-        }
-
-        HeightFieldLayout ResolveLayout()
-        {
-            if (_layoutHost != null && _layoutHost.EnsureLayout())
-                return _layoutHost.Layout;
-            return _layout;
+            _lod.EnsureUpdated(_layout, height);
+            BindMaterialTextures(height, _lod.NormalTexture);
+            DrawLayers(camera, _layout);
         }
 
         void BindMaterialTextures(RenderTexture height, RenderTexture normal)
@@ -89,18 +58,18 @@ namespace HeightFieldLod
 
         void DrawLayers(Camera camera, HeightFieldLayout layout)
         {
-            var meshes = Lod.LodMeshes;
+            var meshes = _lod.LodMeshes;
             if (meshes == null) return;
-            int levels = Lod.LodLevelCount;
+            int levels = _lod.LodLevelCount;
             var bounds = new Bounds(transform.position, new Vector3(layout.TotalWorldWidth, layout.TotalWorldHeight, 50f));
             var castShadows = _castShadows ? ShadowCastingMode.On : ShadowCastingMode.Off;
             for (int lod = 0; lod < levels; lod++)
             {
-                if (Lod.GetInstanceCount(lod) == 0) continue;
-                _mpb.SetBuffer("_ChunkInstances", Lod.InstanceBuffers[lod]);
+                if (_lod.GetInstanceCount(lod) == 0) continue;
+                _mpb.SetBuffer("_ChunkInstances", _lod.InstanceBuffers[lod]);
                 Graphics.DrawMeshInstancedIndirect(
                     meshes[lod], 0, _material, bounds,
-                    Lod.ArgsBuffers[lod], 0, _mpb,
+                    _lod.ArgsBuffers[lod], 0, _mpb,
                     castShadows, true, gameObject.layer,
                     camera, LightProbeUsage.Off, null);
             }
@@ -108,7 +77,7 @@ namespace HeightFieldLod
 
         bool ShouldRender(Camera camera)
         {
-            if (_material == null || Lod?.LodMeshes == null)
+            if (_material == null || _lod?.LodMeshes == null)
                 return false;
             if (camera.cameraType == CameraType.Preview)
                 return false;
--- Packages/jp.nobnak.heightfield-lod/Runtime/Layout/HeightFieldLayoutHost.cs
+++ Packages/jp.nobnak.heightfield-lod/Runtime/Layout/HeightFieldLayoutHost.cs
@@ -1,49 +1,106 @@
-using HeightField;
-using UnityEngine;
-
-namespace HeightFieldLod
-{
-    [DisallowMultipleComponent]
-    public sealed class HeightFieldLayoutHost : MonoBehaviour
-    {
-        [SerializeField] Camera _camera;
-        [SerializeField] int _barrierChunks = 2;
-
-        HeightFieldLayout _layout;
-        int _lastW = -1;
-        int _lastH = -1;
-        float _lastOrtho = -1f;
-
-        public Camera Camera => _camera;
-        public HeightFieldLayout Layout => _layout;
-
-        void Reset()
-        {
-            if (_camera == null)
-                _camera = Camera.main;
-        }
-
-        public bool EnsureLayout() => EnsureLayout(out _);
-
-        public bool EnsureLayout(out bool layoutChanged)
-        {
-            layoutChanged = false;
-            if (_camera == null) return false;
-            if (!NeedsRebuild())
-                return _layout.TexWidth > 0;
-            _layout = HeightFieldLayout.FromCamera(_camera, _barrierChunks);
-            _lastW = _camera.pixelWidth;
-            _lastH = _camera.pixelHeight;
-            _lastOrtho = _camera.orthographicSize;
-            layoutChanged = true;
-            return true;
-        }
-
-        bool NeedsRebuild()
-        {
-            return _camera.pixelWidth != _lastW
-                || _camera.pixelHeight != _lastH
-                || !Mathf.Approximately(_camera.orthographicSize, _lastOrtho);
-        }
-    }
-}
+using System;
+using HeightField;
+using UnityEngine;
+
+namespace HeightFieldLod
+{
+    [DisallowMultipleComponent]
+    [DefaultExecutionOrder(-100)]
+    public sealed class HeightFieldLayoutHost : MonoBehaviour
+    {
+        [SerializeField] Camera _camera;
+        [SerializeField] int _barrierChunks = 2;
+        [SerializeField] MonoBehaviour _heightSourceBehaviour;
+        [SerializeField] HeightFieldLodCompute _lodCompute;
+        [SerializeField] HeightFieldChunkMeshDrawer[] _drawers;
+
+        HeightFieldLayout _layout;
+        int _lastW = -1;
+        int _lastH = -1;
+        float _lastOrtho = -1f;
+
+        public event Action<HeightFieldLayout> LayoutApplied;
+
+        public Camera Camera => _camera;
+        public HeightFieldLayout Layout => _layout;
+
+        IHeightFieldSource HeightSource => _heightSourceBehaviour as IHeightFieldSource;
+
+        void Reset()
+        {
+            if (_camera == null)
+                _camera = Camera.main;
+            ResolveRefs();
+        }
+
+        void OnValidate() => ResolveRefs();
+
+        void OnEnable()
+        {
+            ResolveRefs();
+            InjectDependencies();
+            if (EnsureLayout(out _))
+                ApplyLayout();
+        }
+
+        void OnDisable() => HeightSource?.Release();
+
+        void Update()
+        {
+            if (!EnsureLayout(out bool layoutChanged)) return;
+            if (!layoutChanged && _lodCompute?.LodMeshes != null) return;
+            ApplyLayout();
+        }
+
+        public bool EnsureLayout() => EnsureLayout(out _);
+
+        public bool EnsureLayout(out bool layoutChanged)
+        {
+            layoutChanged = false;
+            if (_camera == null) return false;
+            if (!NeedsRebuild())
+                return _layout.TexWidth > 0;
+            _layout = HeightFieldLayout.FromCamera(_camera, _barrierChunks);
+            _lastW = _camera.pixelWidth;
+            _lastH = _camera.pixelHeight;
+            _lastOrtho = _camera.orthographicSize;
+            layoutChanged = true;
+            return true;
+        }
+
+        void ResolveRefs()
+        {
+            if (_lodCompute == null)
+                _lodCompute = GetComponent<HeightFieldLodCompute>();
+            if (_drawers == null || _drawers.Length == 0)
+                _drawers = GetComponentsInChildren<HeightFieldChunkMeshDrawer>();
+        }
+
+        void InjectDependencies()
+        {
+            if (_drawers == null) return;
+            var lod = (ILodSource)_lodCompute;
+            var height = HeightSource;
+            for (int i = 0; i < _drawers.Length; i++)
+                _drawers[i]?.SetDependencies(lod, height);
+        }
+
+        void ApplyLayout()
+        {
+            if (_layout.TexWidth <= 0) return;
+            HeightSource?.Allocate(_layout);
+            _lodCompute?.Configure(_layout);
+            if (_drawers == null) return;
+            for (int i = 0; i < _drawers.Length; i++)
+                _drawers[i]?.Configure(_layout);
+            LayoutApplied?.Invoke(_layout);
+        }
+
+        bool NeedsRebuild()
+        {
+            return _camera.pixelWidth != _lastW
+                || _camera.pixelHeight != _lastH
+                || !Mathf.Approximately(_camera.orthographicSize, _lastOrtho);
+        }
+    }
+}
